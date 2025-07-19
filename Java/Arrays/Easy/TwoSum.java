718a766652d3a2cdda8ff380c1393c159e7916ba
omkar <omkarbagade16@gmail.com>
2025-07-19 22:46:56 +0530

    01_code_commit: array easy two_sum

---
+++ Java/Arrays/Easy/TwoSum.java
@@ -0,0 +1,24 @@
+package Java.Arrays.Easy;
+
+public class TwoSum {
+    public static int[] twoSum(int[] nums, int target){
+        for (int i=0; i<nums.length; i++ ){
+            for (int j = 0; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return nums;
+    }
+     public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] result = twoSum(nums, target);
+        if (result.length == 2) {
+            System.out.println("\n\nIndices: " + result[0] + " and " + result[1]);
+        } else {
+            System.out.println("No pair found.");
+        }
+    }
+}
