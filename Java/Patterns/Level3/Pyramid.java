661e3ad1827b5aa8d1661909787dfbf55577b131
omkar <omkarbagade16@gmail.com>
2025-08-05 23:28:12 +0530

    more patters and updated readme

--- Java/Patterns/Level3/Pyramid.java
+++ Java/Patterns/Level3/Pyramid.java
@@ -1,14 +1,26 @@
+//     *
+//    ***
+//   *****
+//  *******
+// *********
+
 package Java.Patterns.Level3;
 
 public class Pyramid {
     public static void main(String[] args) {
-        int n=5;
-        for(int i=1; i<=n; i++){
-            for (int j = i; j <= n; j++) {
+        int n = 5; 
+        for (int i = 1; i <= n; i++) { //rows
+
+            //spaces
+            for (int j = i; j < n; j++) {
+                System.out.print(" ");
+            }
+            //stars
+            for (int k = 1; k <= (2 * i - 1); k++) {
                 System.out.print("*");
             }
             System.out.println();
         }
-        
+
     }
 }
--- Java/Patterns/Level3/README.md
+++ Java/Patterns/Level3/README.md
@@ -3,6 +3,7 @@
 This folder contains Level 3 Patterns in Java.
 
 # Problems: 
-- 
+- Pyramid
+- Diamond
 
 
