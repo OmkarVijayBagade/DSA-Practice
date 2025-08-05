15752841bd13037f3044d5e8bf3788db87e2a50d
omkar <omkarbagade16@gmail.com>
2025-08-05 22:56:50 +0530

    added more levels in patterns questions in java

--- Java/Patterns/Level2/README.md
+++ Java/Patterns/Level2/README.md
@@ -3,6 +3,9 @@
 This folder contains Level 2 Patterns in Java.
 
 # Problems: 
-- 
+- NumberTriangle
+- DecreasingNumberTriangle
+- NumberSquare
+- RepeatingNumbers
 
 
---
+++ Java/Patterns/Level3/Pyramid.java
@@ -0,0 +1,13 @@
+package Java.Patterns.Level3;
+
+public class Pyramid {
+    public static void main(String[] args) {
+        int n=5;
+        for(int i=1; i<=n; i++){
+            for (int j = i; j <= n; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
